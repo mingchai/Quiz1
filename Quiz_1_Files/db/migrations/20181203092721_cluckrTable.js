1a13aafb17da9d4c1a32716baf54361407ae97ba
Ming Chai <ming.chai4@gmail.com>
2018-12-03 10:49:23 -0800

    fixed db issue - capital letter caused issue. Simple solution: replace with lowercase rather than escape capitalization

--- Quiz_1_Files/db/migrations/20181203092721_cluckrTable.js
+++ Quiz_1_Files/db/migrations/20181203092721_cluckrTable.js
@@ -1,13 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("cluckrTable", table =>{
+  return knex.schema.createTable("cluckrtable", table =>{
       table.increments("id");
       table.text("username");
-      table.string("image_url")
+      table.string("image_url");
       table.text("content");
       table.timestamp("createdAt").defaultTo(knex.fn.now());
-      table.timestamp("updatedAt");    
-  })
+  });
 };
 
 exports.down = function(knex, Promise) {
